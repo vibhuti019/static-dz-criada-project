85cb16ca2811e8c0ed105bf029fe77936fbc781f
Vibhuti Singh <vibhutisingh037@gmail.com>
2021-05-05 18:41:22 +0530

    new details

--- index.php
+++ index.php
@@ -4,37 +4,49 @@
     $requestedPath = $path[1];
         
     header("Content-Type: application/json");
-    
+    $bodyOfRequest = file_get_contents('php://input');
+    $arrayOfJSON = json_decode($bodyOfRequest, true);
     
     if(preg_match("/driver/i", $requestedPath)){
         switch($requestedPath){
             case "driver-register":
-                echo '{';
-                echo           '"Data" : {';
-                echo           '"orderId" : "1234567",';
-                echo           '"customerName" : "Melisa Tan",';
-                echo           '"customerMobile" : "989496843",';
-                echo           '"deliveryTime" : "31 Aug 12:20PM",';
-                echo           '"dropOffLocation" : "Toa Poyah",';
-                echo           '"dropUnitNumber" : "04-123",';
-                echo           '"pickupLocation" : "Potang Pasir",';
-                echo           '"pickupUnitNumber" : "04-123",';
-                echo           '"requiredVehicle" : "Bike, (Weight < 8kg)",';
-                echo           '"priceOfDelivery" : "400" ';
-                echo '} ';
-                echo '}';
+                if(true){
+                    echo '{';
+                    echo           '"Data" : {';
+                    echo           '"orderId" : "1234567",';
+                    echo           '"customerName" : "Melisa Tan",';
+                    echo           '"customerMobile" : "989496843",';
+                    echo           '"deliveryTime" : "31 Aug 12:20PM",';
+                    echo           '"dropOffLocation" : "Toa Poyah",';
+                    echo           '"dropUnitNumber" : "04-123",';
+                    echo           '"pickupLocation" : "Potang Pasir",';
+                    echo           '"pickupUnitNumber" : "04-123",';
+                    echo           '"requiredVehicle" : "Bike, (Weight < 8kg)",';
+                    echo           '"priceOfDelivery" : "400" ';
+                    echo '} ';
+                    echo '}';
+                }
                 break;
 
             case "driver-login":
-                echo '{';
-                echo '"Data" : {';
-                echo '    "driverId" : "1234567",';
-                echo '    "driverName" : "RobertSoCool",';
-                echo '    "driverMobile" : "989496843",';
-                echo '    "driverEmail" : "robert@cool.com",';
-                echo '    "token" : "akhgfaisufhakuufbskdfg"';
-                echo '}';
-                echo '}';
+                if($arrayOfJSON['driverMobile'] == '8646587689' && $arrayOfJSON['driverPassword'] == 'Password'){
+                    echo '{';
+                    echo '"Data" : {';
+                    echo '    "driverId" : "1234567",';
+                    echo '    "driverName" : "RobertSoCool",';
+                    echo '    "driverMobile" : "989496843",';
+                    echo '    "driverEmail" : "robert@cool.com",';
+                    echo '    "token" : "akhgfaisufhakuufbskdfg"';
+                    echo '}';
+                    echo '}';
+                }
+                else{
+                    echo '{';
+                    echo '"Data" : {';
+                    echo '    "Error" : "Invaild Creds"';
+                    echo '}';
+                    echo '}';
+                }
                 break;
 
             case "driver-forgot-password":
