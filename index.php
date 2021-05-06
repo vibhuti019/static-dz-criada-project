c577ceb379581fea170a55f61cfb13bd244041ba
Vishnu <vishnuprabhu.bvk@gmail.com>
2021-05-06 15:31:15 +0530

    Header Change

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 
     $path = explode('/',$_SERVER['PATH_INFO']);
     $requestedPath = $path[1];
-        
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Origin : *");
-    header("Access-Control-Allow-Methods : GET,POST,PUT,DELETE,OPTIONS");
+
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     header("Content-Type : application/json");
+
     $bodyOfRequest = file_get_contents('php://input');
     $arrayOfJSON = json_decode($bodyOfRequest, true);
     
