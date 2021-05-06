665f081256c07bc97a3c2432fce2dd7ee42610e5
Vibhuti <vibhutisingh037@gmail.com>
2021-05-06 14:11:08 +0000

    .

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
        header('Access-Control-Allow-Origin: *');
        header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
        header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-       header("Content-Type : application/json");
+//        header("Content-Type : application/json");
 
 //         if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {    
 //            die();    
