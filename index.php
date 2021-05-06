e45042d046e806f4a27c508c3c8c23a0737a8b9c
Vibhuti <vibhutisingh037@gmail.com>
2021-05-06 14:10:21 +0000

    neew

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
     $path = explode('/',$_SERVER['PATH_INFO']);
     $requestedPath = $path[1];
 
-//     header('Access-Control-Allow-Origin: http://localhost:4200');
-//     header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
-//     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-//        header("Content-Type : application/json");
+       header('Access-Control-Allow-Origin: *');
+       header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+       header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+       header("Content-Type : application/json");
 
-//     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {    
-//        die();    
-//     }    
+//         if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {    
+//            die();    
+//         }    
 
 
     $bodyOfRequest = file_get_contents('php://input');
@@ -22,6 +22,7 @@
                 if(true){
                     echo json_encode($arrayOfJSON);
                     echo vardump($_REQUEST);
+                    echo vardump($_SERVER);
                 }
                 break;
 
